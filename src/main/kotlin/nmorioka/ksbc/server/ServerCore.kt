cf74638f275ecae55bfe5a548b63f599c27b3516
Morioka Naoki <n.morioka0@gmail.com>
2018-11-27 13:40:29 +0900

    add ServerCore

---
+++ src/main/kotlin/nmorioka/ksbc/server/ServerCore.kt
@@ -0,0 +1,28 @@
+package nmorioka.ksbc.server
+
+class ServerCore {
+    var serverState: ServerState = ServerState.INIT
+
+    fun start() {
+        serverState = ServerState.STANDBY
+    }
+
+    fun joinNetwork() {
+        serverState = ServerState.CONNECTED_TO_NETWORK
+    }
+
+    fun shutdown() {
+        serverState = ServerState.SHUTING_DOWN
+    }
+
+    fun getMyCurrentState(): ServerState {
+        return serverState
+    }
+}
+
+enum class ServerState(val rawValue :Int)  {
+    INIT(0),
+    STANDBY(1),
+    CONNECTED_TO_NETWORK(2),
+    SHUTING_DOWN(3)
+}
