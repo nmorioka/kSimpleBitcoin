bc743c6c38c0eb2bca98c04fe07fa9ac4cba6fca
Morioka Naoki <n.morioka0@gmail.com>
2018-12-03 17:57:21 +0900

    add cm to ServerCore

--- src/main/kotlin/nmorioka/ksbc/core/ServerCore.kt
+++ src/main/kotlin/nmorioka/ksbc/core/ServerCore.kt
@@ -1,22 +1,48 @@
 package nmorioka.ksbc.core
 
-class ServerCore {
-    var serverState: ServerState = ServerState.INIT
+import nmorioka.ksbc.let2
+import nmorioka.ksbc.p2p.ConnectionManager
 
+class ServerCore(val myHost: String, val myPort:Int, val coreNodeHost: String? = null, val coreNodePort: Int? = null) {
+    var serverState: ServerState
+
+    val cm: ConnectionManager
+
+    init {
+        serverState = ServerState.INIT
+        println("Initializing server...")
+        cm = ConnectionManager(myHost, myPort)
+    }
+
+    /**
+     * Coreノードとしての待受を開始する（上位UI層向け)
+     */
     fun start() {
+        cm.start()
         serverState = ServerState.STANDBY
     }
 
-    fun joinNetwork() {
-        serverState = ServerState.CONNECTED_TO_NETWORK
-    }
-
     fun shutdown() {
+        println("Shutdown server...")
+
         serverState = ServerState.SHUTING_DOWN
+        cm.stop()
     }
 
     fun getMyCurrentState(): ServerState {
         return serverState
+    }
+
+    /**
+     * 事前に取得した情報に従い拠り所となる他のCoreノードに接続する（上位UI層向け)
+     */
+    fun joinNetwork() {
+        if (coreNodeHost != null && coreNodePort != null) {
+            serverState = ServerState.CONNECTED_TO_NETWORK
+            cm.joinNetwork(coreNodeHost, coreNodePort)
+        } else {
+            println("This server is runnning as Genesis Core Node...")
+        }
     }
 }
 
@@ -26,3 +52,9 @@ enum class ServerState(val rawValue: Int) {
     CONNECTED_TO_NETWORK(2),
     SHUTING_DOWN(3)
 }
+
+
+
+
+
+
